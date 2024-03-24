cb9c499b3005109cb16368e008ad418543273bcf
billyrare <69784860+Billyrare@users.noreply.github.com>
2024-03-24 23:29:12 +0300

    Add files via upload

---
+++ Lab8/index.dart
@@ -0,0 +1,364 @@
+Container(
+  width: 954,
+  height: 626,
+  decoration: ShapeDecoration(
+    color: Colors.white,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(43),
+    ),
+  ),
+),
+Text(
+  'CAR DEALER SYSTEM',
+  style: TextStyle(
+    color: Colors.black,
+    fontSize: 24,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  ),
+),
+Container(
+  width: 798,
+  height: 48,
+  decoration: ShapeDecoration(
+    shape: RoundedRectangleBorder(
+      side: BorderSide(
+        width: 1,
+        strokeAlign: BorderSide.strokeAlignOutside,
+      ),
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(23),
+        topRight: Radius.circular(23),
+        bottomRight: Radius.circular(6),
+      ),
+    ),
+  ),
+),
+Text(
+  'Производитель',
+  style: TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  ),
+),
+Text(
+  'Модель',
+  style: TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  ),
+),
+Text(
+  'Объем двигателя',
+  style: TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  ),
+),
+Text(
+  'Владелец',
+  style: TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  ),
+),
+Text(
+  'Год',
+  style: TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  ),
+),
+Text(
+  'Цвет',
+  style: TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  ),
+),
+Text(
+  'Дата заезда',
+  style: TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  ),
+),
+Container(
+  width: 798,
+  height: 48,
+  decoration: ShapeDecoration(
+    shape: RoundedRectangleBorder(
+      side: BorderSide(
+        width: 1,
+        strokeAlign: BorderSide.strokeAlignOutside,
+      ),
+    ),
+  ),
+),
+Container(
+  width: 798,
+  height: 48,
+  decoration: ShapeDecoration(
+    shape: RoundedRectangleBorder(
+      side: BorderSide(
+        width: 1,
+        strokeAlign: BorderSide.strokeAlignOutside,
+      ),
+    ),
+  ),
+),
+Container(
+  width: 798,
+  height: 48,
+  decoration: ShapeDecoration(
+    shape: RoundedRectangleBorder(
+      side: BorderSide(
+        width: 1,
+        strokeAlign: BorderSide.strokeAlignOutside,
+      ),
+    ),
+  ),
+),
+Container(
+  width: 798,
+  height: 48,
+  decoration: ShapeDecoration(
+    shape: RoundedRectangleBorder(
+      side: BorderSide(
+        width: 1,
+        strokeAlign: BorderSide.strokeAlignOutside,
+      ),
+    ),
+  ),
+),
+Container(
+  width: 798,
+  height: 48,
+  decoration: ShapeDecoration(
+    shape: RoundedRectangleBorder(
+      side: BorderSide(
+        width: 1,
+        strokeAlign: BorderSide.strokeAlignOutside,
+      ),
+    ),
+  ),
+),
+Container(
+  width: 798,
+  height: 48,
+  decoration: ShapeDecoration(
+    shape: RoundedRectangleBorder(
+      side: BorderSide(
+        width: 1,
+        strokeAlign: BorderSide.strokeAlignOutside,
+      ),
+      borderRadius: BorderRadius.only(
+        bottomLeft: Radius.circular(23),
+        bottomRight: Radius.circular(23),
+      ),
+    ),
+  ),
+),
+Container(
+  width: 53,
+  height: 337,
+  decoration: ShapeDecoration(
+    shape: RoundedRectangleBorder(
+      side: BorderSide(width: 1),
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(23),
+        bottomLeft: Radius.circular(23),
+      ),
+    ),
+  ),
+),
+Text(
+  '1',
+  style: TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  ),
+),
+Text(
+  '2',
+  style: TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  ),
+),
+Text(
+  '3',
+  style: TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  ),
+),
+Text(
+  '4',
+  style: TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  ),
+),
+Text(
+  '5',
+  style: TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  ),
+),
+Text(
+  '6',
+  style: TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  ),
+),
+Container(
+  width: 142,
+  height: 40,
+  child: Stack(
+    children: [
+      Positioned(
+        left: 0,
+        top: 0,
+        child: Container(
+          width: 142,
+          height: 40,
+          decoration: ShapeDecoration(
+            color: Color(0xFF65E05A),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(28),
+            ),
+          ),
+        ),
+      ),
+      Positioned(
+        left: 27,
+        top: 10,
+        child: Text(
+          'ДОБАВИТЬ',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 16,
+            fontFamily: 'Inter',
+            fontWeight: FontWeight.w400,
+            height: 0,
+          ),
+        ),
+      ),
+    ],
+  ),
+),
+Container(
+  width: 142,
+  height: 40,
+  child: Stack(
+    children: [
+      Positioned(
+        left: 0,
+        top: 0,
+        child: Container(
+          width: 142,
+          height: 40,
+          decoration: ShapeDecoration(
+            color: Color(0xFFE05A5A),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(28),
+            ),
+          ),
+        ),
+      ),
+      Positioned(
+        left: 32,
+        top: 10,
+        child: Text(
+          'УДАЛИТЬ',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 16,
+            fontFamily: 'Inter',
+            fontWeight: FontWeight.w400,
+            height: 0,
+          ),
+        ),
+      ),
+    ],
+  ),
+),
+Container(
+  width: 142,
+  height: 40,
+  child: Stack(
+    children: [
+      Positioned(
+        left: 0,
+        top: 0,
+        child: Container(
+          width: 142,
+          height: 40,
+          decoration: ShapeDecoration(
+            color: Color(0xFF2ABCDD),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(28),
+            ),
+          ),
+        ),
+      ),
+      Positioned(
+        left: 25,
+        top: 10,
+        child: Text(
+          'ИЗМЕНИТЬ',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 16,
+            fontFamily: 'Inter',
+            fontWeight: FontWeight.w400,
+            height: 0,
+          ),
+        ),
+      ),
+    ],
+  ),
+)
